79c764839856bbcdf4b8cd317a71d90439c07aa9
baltazardiez7-lab <baltazardiez7@gmail.com>
2026-03-10 13:21:49 -0300

    Update exercise_time.py

--- exercise_time.py
+++ exercise_time.py
@@ -8,3 +8,9 @@ def time():
     3. Segundos restantes
     """
     total_segundos = 3665
+    total_horas = int(total_segundos//3600)
+    print (total_horas)
+    minutos_restantes= (total_segundos//60-total_horas*60)
+    print (minutos_restantes)
+    segundos_restantes= (total_segundos%3600-minutos_restantes*60)
+    print (segundos_restantes)
